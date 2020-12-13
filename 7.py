a7e81b71b186522c772f963ca8b67be77819ed22
clemkeirua <clement@keiruaprod.fr>
2020-12-13 22:28:20 +0100

    parsed 7

--- 7.py
+++ 7.py
@@ -1,25 +1,27 @@
 import re
-file = open('input/7.txt', 'r') 
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+file = open('input/7ex.txt', 'r') 
 data = [i.strip() for i in file.readlines()]
 
+def parse(data):
+	bags = {}
+	for line in data:
+		sep = line.split(" contain ")
+		containees = sep[1].split(", ")
+		container = sep[0][:-5]
+		if container not in bags:
+			bags[container] = {}
+		containees = list(map(lambda x: x.replace(" bags", "").replace(" bag", "").rstrip("."), containees))
+		if containees[0] == "no other":
+			continue
+		else:
+			rebags = list(map(lambda x: re.findall("(\\d+) (.*)", x), containees))
+			for result in rebags:
+				result = result[0]
+				bags[container][result[1]] = int(result[0])
+	return bags
 
-answers = [
-current_answer = ""
-for line in data:
-	if line == "":
-		s = set("".join(current_answer.replace(" ", "")))
-		answers.append(s)
-		current_answer = ""
-	else:
-		current_answer = current_answer + line
-# special case for the last 
-if current_answer != "":
-	s = set("".join(current_answer.replace(" ", "")))
-	answers.append(s)
-
-print(answers)
-p1 = 0
-for a in answers:
-	p1 += len(a)
-print(p1)
-
+bags = parse(data)
+pp.pprint(bags)
