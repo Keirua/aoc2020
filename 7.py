9906212803d91438fe88f6bdeecfe0fb0db428c3
clemkeirua <clement@keiruaprod.fr>
2020-12-13 22:44:19 +0100

    7a

--- 7.py
+++ 7.py
@@ -2,7 +2,7 @@ import re
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-file = open('input/7ex.txt', 'r') 
+file = open('input/7.txt', 'r') 
 data = [i.strip() for i in file.readlines()]
 
 def parse(data):
@@ -25,3 +25,23 @@ def parse(data):
 
 bags = parse(data)
 pp.pprint(bags)
+
+def has_shiny(bags):
+	nb_shiny = 0
+	visited = []
+	has_parents = ["shiny gold"]
+	while len(has_parents) > 0:
+		curr_child = has_parents.pop()
+		for c in bags.keys():
+			if curr_child in bags[c].keys() and c not in visited:
+				nb_shiny = nb_shiny + 1
+				visited.append(c)
+				has_parents.append(c)
+		# print(has_parents)
+
+	return nb_shiny
+
+n = has_shiny(bags)
+print(n)
+
+
