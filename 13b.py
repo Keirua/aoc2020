80dbb511c47b7ba8f1c989f20e460df8199e66c9
clemkeirua <clement@keiruaprod.fr>
2020-12-14 15:32:33 +0100

    extended euclid algorithm

--- 13b.py
+++ 13b.py
@@ -5,20 +5,38 @@ pp = pprint.PrettyPrinter(indent=4)
 file = open('input/13.txt', 'r') 
 data = [i.strip() for i in file.readlines()]
 
-def parse_shuttles(l):
+def parse_constraints(l):
 	buses = l.split(',')
-	offsets = list(range(len(buses)))
-	return [(int(b), o) for (b, o) in zip(buses, offsets) if b != "x"]
+	indices = list(range(len(buses)))
+	return [(int(b), o) for (b, o) in zip(buses, indices) if b != "x"]
 
-all_shuttles = parse_shuttles(data[1])
+all_shuttles = parse_constraints(data[1])
+
+# https://fr.wikipedia.org/wiki/Inverse_modulaire
+def modular_inverse(a, n):
+	pass
+
+# https://fr.wikipedia.org/wiki/Algorithme_d%27Euclide_%C3%A9tendu#Pseudo-code
+def extended_euclid_algorithm(a, b):
+	r, u, v, rp, up, vp = a, 1, 0, b, 0, 1
+	while rp != 0:
+		q = r//rp
+		r, u, v, rp, up, vp = rp, up, vp, r-q*rp, u - q*up, v-q*vp
+	return (r, u, v)
 
 # Finding the solution involves the chinese remainder theorem
 # https://math.stackexchange.com/questions/3006823/is-there-an-algorithm-or-formula-to-find-the-alignment-of-multiple-points
-def earliest(timestamp):
-	pass
+def earliest(shuttle_list):
+	constraints = parse_constraints(shuttle_list)
+	n = 1
+	for c in constraints:
+		n *= c[0]
+	print(extended_euclid_algorithm(23, 120))
+	print(constraints)
+	print(n)
 
 # print(parse_shuttles(data[1]))
-print(parse_shuttles("7,13,x,x,59,x,31,19"))
+earliest("7,13,x,x,59,x,31,19")
 
 # earliest("7,13,x,x,59,x,31,19") == 1068781
 # earliest("17,x,13,19") == 3417
