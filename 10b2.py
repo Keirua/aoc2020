5013d7486d5d8501b0f16dc32dc7462a5b9c63f4
clemkeirua <clement@keiruaprod.fr>
2020-12-14 22:00:28 +0100

    some notes

--- 10b2.py
+++ 10b2.py
@@ -12,19 +12,47 @@ adapters.append(0)
 adapters.sort()
 # Rewrote a solution in order to get dynamic programming
 # https://www.youtube.com/watch?v=cE88K2kFZn0
-DP = {}
-def dp(i):
+
+# dp(i) = the number of ways to reach the end given we currently are at adapter i
+def dp_v1(i):
+	"""
+	A first solution in exponential time (O(fib(n)) ?)
+	"""
+	# From the end, there is only one path
 	if i == len(adapters)-1:
 		return 1
-	if i in DP:
-		return DP[i]
 	ans = 0
+	# one way to get from i to the end is to go from all the valid j to the end, that is to say from dp(j)
+	# dp(i) = \sum_{valid j} dp(j)
 	for j in range(i+1, i+4):
 		if j < len(adapters) and (adapters[j] - adapters[i]) <= 3:
 			ans += dp(j)
+
+	return ans
+
+DP = {}
+def dp_v2(i):
+	"""
+	Another version, in linear time O(n)
+	Time complexity in a memoized DP problem = 
+	  - how many times you call dp (= what is the size of the input)
+	  - how many operations you do in the non-recursive part (here, the for loop)
+	memory complexity = O(n)
+	"""
+	if i == len(adapters)-1:
+		return 1
+	# once you are at a known step, you dont care how you got there
+	if i in DP:
+		return DP[i]
+	ans = 0
+
+	for j in range(i+1, i+4):
+		if j < len(adapters) and (adapters[j] - adapters[i]) <= 3:
+			ans += dp(j)
+	# memoization for later use
 	DP[i] = ans
 
 	return ans
 
 
-print(dp(0))
+print(dp_v2(0))
---
+++ readme.md
@@ -0,0 +1,14 @@
+My solutions to advent of code 2020, in python
+
+ - some of the cool problems with a reasonnably clean solutions are:
+   - 7
+   - 8
+   - 10b (10b2 has some notes)
+   - 13b (solved with chinese remainder theorem, which requires extended gcd)
+
+
+Some cool links:
+ - [the subreddit](reddit.com/r/adventofcode) where the community gathers
+ - [Advent of Code serie in rust](https://fasterthanli.me/series/advent-of-code-2020), by Fasterthanlime. A great introduction to how experienced rustaceans think in the language.
+ - [Visualization](https://www.reddit.com/r/adventofcode/comments/kcpdbi/2020_day_11_part_2luaroblox_waiting_room/) of the waiting room (day 11)
+ - [Visualization](https://www.reddit.com/r/adventofcode/comments/kcw50x/day_7_all_the_bag_rules_in_full_colour/) of the bags (day 7)
