fd8def14baa65263e6ed8ec2565d813690bf7280
clemkeirua <clement@keiruaprod.fr>
2020-12-20 10:40:47 +0100

    generating borders

--- 20.py
+++ 20.py
@@ -9,13 +9,27 @@ def parse(data):
 	tiles = {}
 
 	for i in range(0, len(data), 12):
-		idx = re.findall(r"(\d+)", data[i])[0]
+		idx = int(re.findall(r"(\d+)", data[i])[0])
 		tile = data[i+1:i+11]
 		tiles[idx] = tile
 
 	return tiles
 
+def borders(tile):
+	l = len(tile)
+	b = [
+		# top
+		tile[0],
+		# bottom
+		tile[l-1],
+		# left
+		"".join([tile[j][0] for j in range(l)]),
+		# right
+		"".join([tile[j][l-1] for j in range(l)])
+	]
+	return b
 
 tiles = parse(data)
 pp.pprint(tiles)
 
+print(borders(tiles[2311]))
