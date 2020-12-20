bdfe89324abbdb8b5206db21f5bb19c44192582c
clemkeirua <clement@keiruaprod.fr>
2020-12-20 18:17:49 +0100

    generate all valid permutations

--- 20.py
+++ 20.py
@@ -54,7 +54,33 @@ def possible_neighbours(tile, tiles):
 			neighbours["west"].append(k)
 	return neighbours
 
+def rotate_90(original):
+    """
+    I stole this one here:
+    https://stackoverflow.com/a/496056
+    """
+    # return list(zip(*original[::-1]))
+    return list(map(lambda l: "".join(l), zip(*original[::-1])))
+
+def rotate_m90(original):
+    return list(map(lambda l: "".join(l), list(zip(*original))[::-1]))
+
+def rotate_m180(original):
+    return rotate_m90(rotate_m90(original))
+
+def rotate_m270(original):
+    return rotate_m90(rotate_m90(rotate_m90(original)))
+
+def rotate_180(original):
+	return rotate_90(rotate_90(original))
+
+def rotate_270(original):
+	return rotate_90(rotate_90(rotate_90(original)))
+
 def flip_horizontal(tile):
+	"""
+	flip horizontal = read columns in reverse order
+	"""
 	l = len(tile)
 	out = [list("."* len(tile)) for i in range(l)]
 	for y in range(l):
@@ -65,18 +91,67 @@ def flip_horizontal(tile):
 	return out
 
 def flip_vertical(tile):
+	"""
+	flip horizontal = read lines in reverse order
+	"""
 	return tile[::-1]
 
 tiles = parse(data)
 pp.pprint(tiles)
 
+# print("original tile")
+# pp.pprint(tiles[2311])
+# print("rotate_90")
+# pp.pprint(rotate_90(tiles[2311]))
+# print("rotate_180")
+# pp.pprint(rotate_180(tiles[2311]))
+# print("rotate_270")
+# pp.pprint(rotate_270(tiles[2311]))
+# print("flip_vertical")
+# pp.pprint(flip_vertical(tiles[2311]))
+# print("flip_horizontal")
+# pp.pprint(flip_horizontal(tiles[2311]))
+
+def generate_all_permutations(tile):
+	# Each variant of a tile t can be represented as
+	# rot^r(flip^b(t)), where r € [0, 1, 2, 3] and f € [0, 1]
+	permutations = [
+		tile,
+		flip_vertical(tile),
+		rotate_90(tile),
+		rotate_180(tile),
+		rotate_270(tile),
+		rotate_90(flip_vertical(tile)),
+		rotate_180(flip_vertical(tile)),
+		rotate_270(flip_vertical(tile)),
+	]
+
+	# I was not so sure of that, so a bunch of checks happened
+	# others = [
+	# 	flip_horizontal(tile),
+	# 	rotate_90(flip_horizontal(tile)),
+	# 	rotate_180(flip_horizontal(tile)),
+	# 	rotate_270(flip_horizontal(tile)),
+	# 	rotate_270(flip_horizontal(flip_vertical(tile))),
+	# 	rotate_180(flip_horizontal(flip_vertical(tile))),
+	# 	rotate_90(flip_horizontal(flip_vertical(tile))),
+	# 	rotate_m90(flip_horizontal(tile)),
+	# 	rotate_m180(flip_horizontal(tile)),
+	# 	rotate_m270(flip_horizontal(tile)),
+	# 	rotate_m270(flip_horizontal(flip_vertical(tile))),
+	# 	rotate_m180(flip_horizontal(flip_vertical(tile))),
+	# 	rotate_m90(flip_horizontal(flip_vertical(tile)))
+	# ]
+	# for i,c in enumerate(others):
+	# 	if c not in permutations:
+	# 		print("missing {}".format(i))
+
+	return permutations
+
+print("borders of the original tile")
 print(borders(tiles[2311]))
-print("original tile")
-pp.pprint(tiles[2311])
-print("flip_vertical")
-pp.pprint(flip_vertical(tiles[2311]))
-print("flip_horizontal")
-pp.pprint(flip_horizontal(tiles[2311]))
+pp.pprint(generate_all_permutations(tiles[2311]))
+
 print()
 neighbours = {}
 for t_id in tiles.keys():
