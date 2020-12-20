70b2fa976799cd36575aef27885259b4e8d0e427
clemkeirua <clement@keiruaprod.fr>
2020-12-20 17:16:19 +0100

    generate the list of neighbours in every direction

--- 20.py
+++ 20.py
@@ -15,6 +15,11 @@ def parse(data):
 
 	return tiles
 
+TOP = 0
+BOTTOM = 1
+LEFT = 2
+RIGHT = 3
+
 def borders(tile):
 	l = len(tile)
 	b = [
@@ -29,7 +34,36 @@ def borders(tile):
 	]
 	return b
 
+
+
+def possible_neighbours(tile, tiles):
+	neighbours = {
+		"north": [],
+		"south": [],
+		"east": [],
+		"west": []
+	}
+	b = borders(tile)
+	for _, (k,v) in enumerate(tiles.items()):
+		bn = borders(v)
+		if bn[BOTTOM] == b[TOP]:
+			neighbours["north"].append(k)
+		if bn[TOP] == b[BOTTOM]:
+			neighbours["south"].append(k)
+		if bn[LEFT] == b[RIGHT]:
+			neighbours["east"].append(k)
+		if bn[RIGHT] == b[LEFT]:
+			neighbours["west"].append(k)
+	return neighbours
+
+
 tiles = parse(data)
 pp.pprint(tiles)
 
 print(borders(tiles[2311]))
+neighbours = {}
+for t_id in tiles.keys():
+ 	n = possible_neighbours(tiles[t_id], tiles)
+ 	neighbours[t_id] = n
+
+pp.pprint(neighbours)
