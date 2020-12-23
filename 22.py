f55b7dc1a80788f665f9c4be7f0bdc3b0ab4ebb4
clemkeirua <clement@keiruaprod.fr>
2020-12-23 20:15:09 +0100

    solved day22b

--- 22.py
+++ 22.py
@@ -44,6 +44,75 @@ def run_game(players):
 		if len(players[1]) == 0:
 			return compute_score(players[0])
 
-score = run_game(players)
-print(score)
-pp.pprint(players)
+def hash_hand(hand):
+	return ",".join(map(str, hand))
+
+def hash_hands(players):
+	return "{}-{}".format(hash_hand(players[0]), hash_hand(players[1]))
+
+def display_state(players, depth, nb_rounds):
+	tabs = "\t" * (depth-1)
+	print("{}-- Round {} (Game {}) --".format(tabs, nb_rounds, depth))
+	print("{}Player’s 1 deck: {}".format(tabs, hash_hand(players[0])))
+	print("{}Player’s 2 deck: {}".format(tabs, hash_hand(players[1])))
+
+	print("{}Player’s 1 plays: {}".format(tabs, players[0][-1]))
+	print("{}Player’s 2 plays: {}".format(tabs, players[1][-1]))
+
+	print()
+
+def run_game_recursive(players, hashes=[], depth=0):
+	nb_rounds = 0
+	while True:
+		nb_rounds = nb_rounds +1
+		# Before either player deals a card, if there was a previous round
+		# in this game that had exactly the same cards in the same order
+		# in the same players' decks, the game instantly ends
+		# in a win for player 1.
+		hashed_hand = hash_hands(players)
+		if hashed_hand in hashes:
+			return compute_score(players[0]), 0
+		hashes.append(hashed_hand)
+		# display_state(players, depth, nb_rounds)
+
+		top0 = players[0].pop()
+		top1 = players[1].pop()
+		cards = [max(top0, top1), min(top0, top1)]
+		if len(players[0]) < top0 or len(players[1]) < top1:
+			# at least one player must not have enough cards left in their deck
+			# to recurse; the winner of the round is the player with the higher-value card.
+			winner = 0
+			if top1 > top0:
+				winner = 1
+			players[winner].appendleft(cards[0])
+			players[winner].appendleft(cards[1])
+			if len(players[0]) == 0:
+				return compute_score(players[1]), 1
+			if len(players[1]) == 0:
+				return compute_score(players[0]), 0
+			continue
+		# If both players have at least as many cards remaining in their deck
+		# as the value of the card they just drew, the winner of the round is determined
+		# by playing a new game of Recursive Combat.
+		sub_cards0 = deque([players[0][-(i+1)] for i in range(top0)][::-1])
+		sub_cards1 = deque([players[1][-(i+1)] for i in range(top1)][::-1])
+		score, winner = run_game_recursive([sub_cards0, sub_cards1], [], depth+1)
+		
+		if winner == 0:
+			players[0].appendleft(top0)
+			players[0].appendleft(top1)
+		elif winner == 1:
+			players[1].appendleft(top1)
+			players[1].appendleft(top0)
+
+		if len(players[0]) == 0:
+			return compute_score(players[1]), 1
+		if len(players[1]) == 0:
+			return compute_score(players[0]), 0
+
+# score = run_game(players)
+# print(score)
+
+score2, winner = run_game_recursive(players, [], 1)
+# 34931 is too low
+print(score2, winner)
