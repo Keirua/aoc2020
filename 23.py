6af057d1218045d2becaecb803def30c25cf1ab8
clemkeirua <clement@keiruaprod.fr>
2020-12-27 12:03:05 +0100

    started writing a custom linked list

--- 23.py
+++ 23.py
@@ -1,12 +1,41 @@
+import re
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 sample_input = "389125467"
 real_input = "853192647"
 
+
+class Node:
+	def __init__(self, v = None):
+		self.value = v
+		self.prev = None
+		self.next = None
+
+	def __repr__(self):
+		return "{} = {} - prev = {}, next = {}".format(id(self), self.value, id(self.prev), id(self.next))
+
+class NodeList:
+	def __init__(self, data):
+		self.data = data
+		self.nodes = []
+		l = len(data)
+		for d in data:
+			self.nodes.append(Node(d))
+		for i in range(l):
+			self.nodes[i].next = self.nodes[(i+1)%l]
+			self.nodes[i].prev = self.nodes[(i-1+l)%l]
+	
+	def __repr__(self):
+		return "\n".join(str(s) for s in self.nodes)
+
 def parse(input):
-	return list(map(int, list(input)))
+	data = list(map(int, list(input)))
+	return data, NodeList(data)
+
 
 def run_game(cups, nb_turns):
-	cups = parse(cups)
+	cups,nodelist = parse(cups)
 	for curr_turn in range(nb_turns):
 		next3 = [cups[(curr_turn+ 1+i) % len(cups)] for i in range(3)]
 		print("-- move {}--".format(curr_turn+1))
@@ -48,6 +77,9 @@ def run_game(cups, nb_turns):
 # deck = parse(sample_input)
 # deck = parse(real_input)
 
+cups,nodelist = parse(sample_input)
+pp.pprint(nodelist)
+
 # assert(run_game("389125467", 10) == "92658374")
 # assert(run_game("389125467", 100) == "67384529")
-print(run_game(sample_input, 10))
+# print(run_game(sample_input, 10))
